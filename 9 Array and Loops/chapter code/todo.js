b0b1092616734ccf2faf529cf169c1031881ab89
omaisahmad <omaisahmad10.dev@gmail.com>
2026-04-01 16:18:04 +0500

    Todo App

--- 9 Array and Loops/chapter code/todo.js
+++ 9 Array and Loops/chapter code/todo.js
@@ -1,13 +1,26 @@
-let todoList=[];
+let todoList=[
+
+      {item: 'by milk',
+      dueDate: '01/04/2026'},
+  
+      {item: ' Go to college',
+      dueDate: '01/04/2026'}
+    ];
 
 displayItems();
 function addtodo(){
 
   let inputElements = document.querySelector('#todo-input');
-  let todoitem=inputElements.value;
+  
+  let DateElements = document.querySelector('#todo-date');
 
-  todoList.push(todoitem);
+  let todoitem=inputElements.value;
+  let tododate=DateElements.value;
+
+  todoList.push({ item:  todoitem, dueDate : tododate});
    inputElements.value = '';
+   DateElements.value = '';
+
 
    
 displayItems();
@@ -17,20 +30,20 @@ function displayItems(){
   let ContainerElement=document.querySelector('.todo-container');
  
   let newHTMl = '';
-
   for (let i = 0; i < todoList.length; i++) {
-
-    newHTMl += `
-        <div>
-          <span>${todoList[i]} <span>
-          <button>Delete</button>
-        </div>
-    `
-      
-       
-
+     let {item , dueDate}=todoList[i];
     
+    newHTMl += `
+           
+          <div> <span>${item} <span></div>
+         <div> <span>${dueDate} <span></div>
+
+     <div ><button class="btun-delete"  onclick="todoList.splice(${i} , 1);
+            displayItems();
+         
+            ">Delete</button></div>
+    `;
   }
   ContainerElement.innerHTML=newHTMl;
-}
+};
 
