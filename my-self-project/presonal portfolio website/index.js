e8b59b6ccd1b0730cca7fb8f6317eef152abdbe5
Omais Ahmad <omaisahmad10.dev@gmail.com>
2026-09-19 22:44:44 -0700

    personal portfolio completed

--- my-self-project/presonal portfolio website/index.js
+++ my-self-project/presonal portfolio website/index.js
@@ -5,12 +5,39 @@ $(document).ready(function(){
     }else{
       $('.navabar').removeClass("sticky");
     }
+    if(this.scrollY > 500){
+      $('.scroll-up-down').addClass("show")
+    }else{
+      
+      $('.scroll-up-down').removeClass("show")
+    }
     
-  })
+  });
+
+  // scroll menu script
+
+    $('.scroll-up-down').click(function(){
+      $('html').animate({scrollTop : 0});
+    });
+
   $('.menu-btn').click(function(){
     $('.navabar .menu').toggleClass('active')
-    $('.menu-btn i' ).toggleClass('active')
-  })
+    $('.menu-btn i').toggleClass('active')
+  });
+
+  // Typing animation script
+  var typed = new Typed (".typing",{
+    strings:["YouTuber", "Developer" , "Freelancer", "Designer","Blogger"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true,
+  });
+   var typed = new Typed (".typing-2",{
+    strings:["YouTuber", "Developer" , "Freelancer", "Designer","Blogger"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true,
+  });
 
   // owl carousel script
 
